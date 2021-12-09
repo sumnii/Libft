0d5759127af8dd30c5570ed66a1396446746bd21
Sumin Song <sumsong@c2r1s1.42seoul.kr>
2021-12-09 17:40:59 +0900

    FEAT libft.h

---
+++ libft/libft.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sumsong <sumsong@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/09 17:14:35 by sumsong           #+#    #+#             */
+/*   Updated: 2021/12/09 17:19:04 by sumsong          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+#include <stdlib.h>
+#include <unistd.h>
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+
+size_t	ft_strlen(const char *s);
+
+#endif
