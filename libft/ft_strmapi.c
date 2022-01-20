155b62f251cd8a1bd80986638c80eb8b8d9d826a
Sumin Song <sumsong@c1r1s2.42seoul.kr>
2022-01-20 22:43:46 +0900

    FIX ft_strmapi [by unit-test]

--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: sumsong <sumsong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/07 14:12:28 by sumsong           #+#    #+#             */
-/*   Updated: 2022/01/18 21:19:47 by sumsong          ###   ########.fr       */
+/*   Updated: 2022/01/20 22:19:07 by sumsong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,18 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
 	char			*str;
 	unsigned int	i;
+	unsigned int	len;
 
 	i = 0;
-	str = ft_strdup(s);
-	while (str[i])
+	len = ft_strlen(s);
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (str == 0)
+		return (0);
+	while (i < len)
 	{
-		str[i] = f(i, str[i]);
+		str[i] = f(i, s[i]);
 		++i;
 	}
+	str[i] = 0;
 	return (str);
 }
